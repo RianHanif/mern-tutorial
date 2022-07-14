47e11e1e0d4b9ca578db35ed2204bac0bc3120e9
DewiPutriS <dewiputrikaneki@gmail.com>
2022-07-14 21:17:57 +0700

    ubah controller

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,9 +1,35 @@
-const { status } = require("express/lib/response")
-
-const get Goals = (req, res) =>{
-    res status(200).json({message: 'Get Goals'})
+//@desc     Get goals
+//@route    GET /api/goals
+//@Access   Private
+const getGoals = (req,res) => {
+    res.status(200).json({message: 'Get Goals'})
 }
 
+//@desc     set goals
+//@route    POST /api/goals
+//@Access   Private
+const setGoal = (req,res) => {
+    res.status(200).json({message: 'Set Goal'})
+}
+
+//@desc     Update goals
+//@route    PUT /api/goals/:id
+//@Access   Private
+const updateGoal = (req,res) => {
+    res.status(200).json({message: `Update Goal ${req.params.id}`})
+}
+
+//@desc     Delete goals
+//@route    DELETE /api/goals/:id
+//@Access   Private
+const deleteGoal = (req,res) => {
+    res.status(200).json({message: `Delete Goal ${req.params.id}`})
+}
+
+
 module.exports = {
-    getGoals
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal
 }
