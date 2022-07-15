3e44bea840ca2bf2ca9424afb736e825a27393f1
RianHanif <rianzayyanh@gmail.com>
2022-07-15 23:12:31 +0700

    Protecting Routes & Getme

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -37,6 +37,7 @@ const registerUser = asyncHandler(async (req, res) => {
             _id: user.id,
             name: user.name,
             email: user.email,
+            token : generateToken(user._id),
         })
     } else {
         res.status(400)
@@ -68,10 +69,23 @@ const loginUser = asyncHandler(async (req, res) => {
 
 //@desc     Get User Dtaa
 //@route    GET /api/users/me
-//@Access   Public
+//@Access   Private
 const getMe = asyncHandler(async (req, res) => {
-    res.json({ Message: "Pengguna display"})
+    const { _id, name, email } = await User.findById(req.user.id)
+
+    res.status(200).json({
+        id: _id,
+        name,
+        email,
+    })
 })
+
+//Generate JWT
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: "30d",
+    })
+}
 
 module.exports = { 
     registerUser,
