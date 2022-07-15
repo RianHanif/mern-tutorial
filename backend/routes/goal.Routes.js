c1eee41ef36672a2bac0e1f552a61fb46f4db355
RianHanif <rianzayyanh@gmail.com>
2022-07-15 13:26:06 +0700

    Connecting Controller Functions

--- backend/routes/goal.Routes.js
+++ backend/routes/goal.Routes.js
@@ -1,20 +1,15 @@
 const express = require('express')
-const res = require('express/lib/response')
 const router = express.Router()
 
-const { getGoals } = require('../controllers/goalController')
+const {
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal
+} = require('../controllers/goalController')
 
-router.get('/',getGoals)
+router.route('/').get(getGoals).post(setGoal)
+router.route('/:id').put(updateGoal).delete(deleteGoal)
 
-router.post('/',(req,res)=>{
-    res.status(200).json({message:'Set Goal'})
-})
 
-router.put('/:id',(req,res)=>{
-    res.status(200).json({message:`Update Goal ${req.params.id}`})
-})
-
-router.delete('/:id',(req,res)=>{
-    res.status(200).json({message:`Delete Goal ${req.params.id}`})
-})
 module.exports = router
